8548b0fc6e657fee2ee11fb15b74459c43922d1d
tastefulblues <maxjh28@gmail.com>
2026-08-25 07:52:57 -0600

    Stray arrow icon removed

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -78,7 +78,7 @@ export default function Projects() {
             target="_blank"
             rel="noreferrer"
           >
-            Code →
+            Code
           </a>
         </div>
       </article>
